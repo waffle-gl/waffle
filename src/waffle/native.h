2459c913fa49c345f56c9ef6a5035b37cbb199c7
Chad Versace <chad.versace@linux.intel.com>
2012-04-03 15:30:55 -0700

    native: Define native types and dispatch table
    Define `union {platform}_{name}` for platform=droid,glx,mac,wayland,xegl
    and name=platform,display,config,context,window.
    
    Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

---
+++ src/waffle/native.h
@@ -0,0 +1,125 @@
+// Copyright 2012 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define NATIVE_UNION(name) \
+    struct droid_##name; \
+    struct glx_##name; \
+    struct mac_##name; \
+    struct wayland_##name; \
+    struct xegl_##name; \
+    \
+    union native_##name { \
+        struct droid_##name *droid; \
+        struct glx_##name *glx; \
+        struct mac_##name *mac; \
+        struct wayland_##name *wayland; \
+        struct xegl_##name *xegl; \
+    }
+
+NATIVE_UNION(platform);
+NATIVE_UNION(config);
+NATIVE_UNION(context);
+NATIVE_UNION(display);
+NATIVE_UNION(window);
+
+/// @brief Allocate a `union native_?`.
+///
+/// @param x must have type `union native_?`.
+/// @param platform is one of droid, glx, mac, wayland, xegl.
+#define NATIVE_ALLOC(x, platform) \
+    do { \
+        x = calloc(1, sizeof(*x) + sizeof(*x->platform)); \
+        x->platform = (void*) (x + 1); \
+    } while (0)
+
+
+struct native_dispatch {
+    /// @defgroup native_display
+    /// @{
+
+    union native_display*
+    (*display_connect)(
+            union native_platform *platform,
+            const char *name);
+
+    bool
+    (*display_disconnect)(union native_display *self);
+
+    /// @}
+    /// @defgroup native_config
+    /// @{
+
+    union native_config*
+    (*config_choose)(
+            union native_display *dpy,
+            const int32_t attrib_list[]);
+
+    bool
+    (*config_destroy)(union native_config *self);
+
+    /// @}
+    /// @defgroup native_context
+    /// @{
+
+    union native_context*
+    (*context_create)(
+            union native_config *config,
+            union native_context *shared_context);
+
+    bool
+    (*context_destroy)(union native_context *self);
+
+    /// @}
+    /// @defgroup native_window
+    /// @{
+
+    union native_window*
+    (*window_create)(
+            union native_config *config,
+            int width,
+            int height);
+
+    bool
+    (*window_destroy)(union native_window *self);
+
+    bool
+    (*window_swap_buffers)(union native_window *self);
+
+    /// @}
+    /// @defgroup native gl misc
+    /// @{
+
+    bool
+    (*make_current)(
+            union native_window *window,
+            union native_context *ctx);
+
+    void*
+    (*get_proc_address)(
+            union native_platform *platform,
+            const char *name);
+
+    void*
+    (*dlsym_gl)(
+            union native_platform *platform,
+            const char *name);
+
+    /// @}
+};
