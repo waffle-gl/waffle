ff642f59a2f59bddf468592e6386759bd118b989
Chad Versace <chad.versace@linux.intel.com>
2012-04-03 21:25:14 -0700

    core: Add wcore_platform
    Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

---
+++ src/waffle/core/wcore_platform.c
@@ -0,0 +1,90 @@
+// Copyright 2012 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "wcore_platform.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <waffle/waffle_enum.h>
+#include <waffle/x11_egl/xegl_platform.h>
+
+#include "wcore_error.h"
+
+static uint64_t id_counter = 0;
+
+struct wcore_platform*
+wcore_platform_create(int platform, int gl_api)
+{
+    struct wcore_platform *self;
+
+    self = calloc(1, sizeof(*self));
+    if (!self) {
+        wcore_error(WAFFLE_OUT_OF_MEMORY);
+        return NULL;
+    }
+
+    self->id = ++id_counter;
+    if (self->id == 0) {
+        fprintf(stderr, "waffle: error: internal counter wrapped around to 0");
+        abort();
+    }
+
+    self->gl_api = gl_api;
+
+    switch (platform) {
+        case WAFFLE_PLATFORM_X11_EGL:
+            self->native = xegl_platform_create(gl_api, &self->dispatch);
+            if (!self->native)
+                goto error;
+            break;
+        default:
+            wcore_error_internal("bad value for platform (0x%x)", platform);
+            goto error;
+    }
+
+    self->native_tag = platform;
+
+    return self;
+
+error:
+    WCORE_ERROR_DISABLED({
+        wcore_platform_destroy(self);
+    });
+    return NULL;
+}
+
+bool
+wcore_platform_destroy(struct wcore_platform *self)
+{
+    bool ok = true;
+
+    if (!self)
+        return true;
+
+    switch (self->native_tag) {
+        case WAFFLE_PLATFORM_X11_EGL:
+            ok &= xegl_platform_destroy(self->native);
+            break;
+        default:
+            ok = false;
+            wcore_error_internal("bad value for wcore_platform._native_tag "
+                                 "(0x%x)", self->native_tag);
+            break;
+    }
+
+    return ok;
+}
---
+++ src/waffle/core/wcore_platform.h
@@ -0,0 +1,41 @@
+// Copyright 2012 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+struct native_dispatch;
+union native_platform;
+
+struct wcore_platform {
+    /// @brief Each instance has a unique id.
+    uint64_t id;
+
+    /// @brief One of WAFFLE_{GL,GLES1,GLES2}.
+    int gl_api;
+
+    /// @brief One of WAFFLE_PLATFORM_*. Denotes type of `native`.
+    int native_tag;
+
+    union native_platform *native;
+    const struct native_dispatch *dispatch;
+};
+
+struct wcore_platform*
+wcore_platform_create(int platform, int gl_api);
+
+bool
+wcore_platform_destroy(struct wcore_platform *self);
