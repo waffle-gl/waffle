a0d07c4411d4f688ab237802fb85012a2e3fb53d
Chad Versace <chad.versace@linux.intel.com>
2012-11-15 10:45:07 -0800

    waffle: Bump version to waffle-1.2.1
    Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

--- Android.mk
+++ Android.mk
@@ -8,7 +8,7 @@ waffle_top := $(LOCAL_PATH)
 #
 waffle_major_version := 1
 waffle_minor_version := 2
-waffle_patch_version := 0
+waffle_patch_version := 1
 
 waffle_android_major_version := $(word 1, $(subst ., , $(PLATFORM_VERSION)))
 waffle_android_minor_version := $(word 2, $(subst ., , $(PLATFORM_VERSION)))
