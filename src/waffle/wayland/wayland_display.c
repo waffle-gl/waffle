7a4fc241297e62a3279bf3ce47fd4ffe7e1f27ca
Chad Versace <chad.versace@linux.intel.com>
2013-01-27 20:57:13 -0800

    wayland: Rename method of registry listener
    The function name wayland_display_listener(), though previously accurate,
    became a misnomer when Waffle was updated for Wayland 1.0. It's now
    a registry listener.
    
    Rename wayland_display_listener() to registry_listener_global().
    
    Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

--- src/waffle/wayland/wayland_display.c
+++ src/waffle/wayland/wayland_display.c
@@ -58,7 +58,7 @@ wayland_display_destroy(struct wcore_display *wc_self)
 }
 
 static void
-wayland_display_listener(void *data,
+registry_listener_global(void *data,
                          struct wl_registry *registry,
                          uint32_t name,
                          const char *interface,
@@ -77,7 +77,7 @@ wayland_display_listener(void *data,
 }
 
 static const struct wl_registry_listener registry_listener = {
-                 .global = wayland_display_listener,
+                 .global = registry_listener_global,
                  .global_remove = NULL,
 };
 
