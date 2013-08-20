4915962cb59ffa7c7c0eae799595fca5f4ca5ba7
Chad Versace <chad.versace@linux.intel.com>
2013-08-19 22:33:33 -0700

    cmake: Refactor definition of internal options
    Move definition of internal options into a new module
    WaffleDefineInternalOptions.
    
    This change belongs to a series that cleans up the toplevel CMakeLists
    by decompositing it into modules.
    
    Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

---
+++ cmake/Modules/WaffleDefineInternalOptions.cmake
@@ -0,0 +1,11 @@
+if(waffle_has_wayland OR waffle_has_x11_egl OR waffle_has_gbm)
+    set(waffle_has_egl TRUE)
+else(waffle_has_wayland OR waffle_has_x11_egl)
+    set(waffle_has_egl FALSE)
+endif()
+
+if(waffle_has_glx OR waffle_has_x11_egl)
+    set(waffle_has_x11 TRUE)
+else()
+    set(waffle_has_x11 FALSE)
+endif()
