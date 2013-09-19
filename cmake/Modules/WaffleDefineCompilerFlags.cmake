waffle-gl/waffle

commit cbab09dc0e6eb8fcea40db92319305d5e136d4b9

cmake: Compile with stricter error checking
Add -Werror=incompatible-pointer-types and -Werror=int-conversion to the
compiler flags.

I'm surprised that gcc doesn't enable them by default.

Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

cmake/Modules/WaffleDefineCompilerFlags.cmake
@@ -28,6 +28,8 @@ include(WaffleCheckThreadLocalStorage)
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --std=c99")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=implicit-function-declaration")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=incompatible-pointer-types")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=int-conversion")
 
 if(waffle_on_linux)
     # On MacOS, the SSE2 headers trigger this error.

commit 7b925cf2033b4536684c86b10755f320c134451d

cmake: Test each compiler flag
Add non-trival C compiler flags only if check_c_compiler_flag()
succeeds.

This fixes the build with 4.8.1, which was broken by

 commit cbab09dc0e6eb8fcea40db92319305d5e136d4b9
 cmake: Compile with stricter error checking

 Add -Werror=incompatible-pointer-types and -Werror=int-conversion
 to the compiler flags.

Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

cmake/Modules/WaffleDefineCompilerFlags.cmake
@@ -23,24 +23,32 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+include(CheckCCompilerFlag)
 include(WaffleCheckThreadLocalStorage)
+
+function(waffle_add_c_flag flag var)
+    check_c_compiler_flag("${flag}" ${var})
+    if(${var})
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}" PARENT_SCOPE)
+    endif()
+endfunction()
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --std=c99")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=implicit-function-declaration")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=incompatible-pointer-types")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=int-conversion")
-
-if(waffle_on_linux)
-    # On MacOS, the SSE2 headers trigger this error.
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=missing-prototypes")
-endif()
-
 set(CMAKE_C_FLAGS_DEBUG "-g3 -O0 -DDEBUG")
 
 # Use '-g1' to produce enough debug info for generating backtraces, but not
 # enough for single-stepping.
 set(CMAKE_C_FLAGS_RELEASE "-g1 -O2 -DNDEBUG")
+
+waffle_add_c_flag("-Werror=implicit-function-declaration" WERROR_IMPLICIT_FUNCTION_DECLARATION)
+waffle_add_c_flag("-Werror=incompatible-pointer-types" WERROR_INCOMPATIBLE_POINTER_TYPES)
+waffle_add_c_flag("-Werror=int-conversion" WERROR_INT_CONVERSION)
+
+if(waffle_on_linux)
+    # On MacOS, the SSE2 headers trigger this error.
+    waffle_add_c_flag("-Werror=missing-prototypes" WERROR_MISSING_PROTOTYPES)
+endif()
 
 waffle_check_thread_local_storage()
 