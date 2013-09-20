a493229779e438973028664192cd312f2b0e0b1b
Juha-Pekka Heikkila <juha-pekka.heikkila@linux.intel.com>
2013-09-20 10:59:59 +0300

    Android: fix examples/gl_basic for android
    Android.mk needed few path changes and -c99 flag to allow
    examples/gl_basic to be compiled for android.
    
    Signed-off-by: Juha-Pekka Heikkila <juha-pekka.heikkila@linux.intel.com>
    Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

--- examples/Android.mk
+++ examples/Android.mk
@@ -7,13 +7,14 @@ LOCAL_MODULE:= gl_basic
 LOCAL_CFLAGS:= \
         -DANDROID_STUB \
         -DWAFFLE_HAS_ANDROID \
+        -std=c99 \
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include/
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../src/
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include/waffle/
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../src/waffle/
 
 LOCAL_SRC_FILES:= \
     gl_basic.c \
 
-LOCAL_SHARED_LIBRARIES := libwaffle
+LOCAL_SHARED_LIBRARIES := libwaffle-1
 
 include $(BUILD_EXECUTABLE)
