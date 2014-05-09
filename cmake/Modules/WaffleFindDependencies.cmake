30eed086ff91f814d015b3f21ec111ea9d498f37
Chad Versace <chad.versace@linux.intel.com>
2014-05-09 10:00:56 -0700

    cmake: Replace pkg_check_modules with waffle_pkg_config
    Define a new macro waffle_pkg_config, which replaces each occurence of
    pkg_check_modules.
    
    Macro waffle_pkg_config is a smart wrapper around CMake's standard
    pkg_check_modules macro.
    
    waffle_pkg_config fixes a fatal flaw of pkg_check_modules.  Unlike
    pkg_check_modules, waffle_pkg_config calls pkg-config and checks for the
    requested package *each* time the macro is called.
    
    Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

--- cmake/Modules/WaffleFindDependencies.cmake
+++ cmake/Modules/WaffleFindDependencies.cmake
@@ -25,6 +25,36 @@
 
 find_package(PkgConfig)
 
+#
+# Macro waffle_pkg_config is a smart wrapper around CMake's standard
+# pkg_check_modules macro.
+#
+# waffle_pkg_config fixes a fatal flaw of pkg_check_modules.  Unlike
+# pkg_check_modules, waffle_pkg_config calls pkg-config and checks for the
+# requested package *each* time the macro is called.
+#
+# Flaws in pkg_check_modules
+# --------------------------
+# For each package prefix (that is, the first argument to pkg_check_modules),
+# pkg_check_modules calls pkg-config only if no previous invocation succeeded
+# in finding the package.  Therefore, if someone increments the required
+# version parameter of some call to pkg_check_modules, then pkg_check_modules
+# does not check for the new version of the package if a previous invocation of
+# CMake succeeded in finding the older version.
+#
+# For example, consider the following patch to some CMakeLists.txt.
+#     - pkg_check_modules(FOO REQUIRED foo>=1.0)
+#     + pkg_check_modules(FOO REQUIRED foo>=2.0)
+# Suppose that the system has foo-1.0 installed but not foo-2.0; and that
+# a previous invocation of CMake suceeded in finding foo-1.0. Future
+# invocations of CMake will continue to succeed even though the system lacks
+# foo-2.0.
+#
+macro(waffle_pkg_config ...)
+    unset(${ARGV0}_FOUND CACHE)
+    pkg_check_modules(${ARGV})
+endmacro()
+
 if(waffle_on_mac)
     find_package(OpenGL REQUIRED)
     find_library(COCOA_FRAMEWORK Cocoa REQUIRED)
@@ -32,23 +62,23 @@ if(waffle_on_mac)
 endif()
 
 if(waffle_has_egl)
-    pkg_check_modules(egl REQUIRED egl)
+    waffle_pkg_config(egl REQUIRED egl)
 endif()
 
 if(waffle_has_glx)
-    pkg_check_modules(gl REQUIRED gl)
+    waffle_pkg_config(gl REQUIRED gl)
 endif()
 
 if(waffle_has_wayland)
-    pkg_check_modules(wayland-client REQUIRED wayland-client>=1)
-    pkg_check_modules(wayland-egl REQUIRED wayland-egl>=9.1)
+    waffle_pkg_config(wayland-client REQUIRED wayland-client>=1)
+    waffle_pkg_config(wayland-egl REQUIRED wayland-egl>=9.1)
 endif()
 
 if(waffle_has_x11)
-    pkg_check_modules(x11-xcb REQUIRED x11-xcb)
+    waffle_pkg_config(x11-xcb REQUIRED x11-xcb)
 endif()
 
 if(waffle_has_gbm)
-    pkg_check_modules(gbm REQUIRED gbm)
-    pkg_check_modules(libudev REQUIRED libudev)
+    waffle_pkg_config(gbm REQUIRED gbm)
+    waffle_pkg_config(libudev REQUIRED libudev)
 endif()
