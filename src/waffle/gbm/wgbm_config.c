waffle-gl/waffle

commit 2bb939095c1d09c79dad5ab5da123ec77eb4ea61

gbm: Use EGLConfig to select the correct gbm format
chadv: As of Mesa 10.4.3, there exists a Mesa bug that prevents this fix
from being a real fix. eglGetConfigAttrib(EGL_NATIVE_VISUAL_ID) always
returns a GBM format without alpha even when the EGLConfig contains
alpha.

Reviewed-by: Chad Versace <chad.versace@intel.com>

src/waffle/gbm/wgbm_config.c
@@ -25,6 +25,8 @@
 
 #include "wcore_error.h"
 
+#include "wegl_platform.h"
+
 #include "wgbm_config.h"
 #include "wgbm_display.h"
 
@@ -33,7 +35,8 @@ wgbm_config_choose(struct wcore_platform *wc_plat,
                    struct wcore_display *wc_dpy,
                    const struct wcore_config_attrs *attrs)
 {
-    if (wgbm_config_get_gbm_format(attrs) == 0) {
+    struct wcore_config *config = wegl_config_choose(wc_plat, wc_dpy, attrs);
+    if (wgbm_config_get_gbm_format(wc_plat, wc_dpy, config) == 0) {
         wcore_errorf(WAFFLE_ERROR_UNSUPPORTED_ON_PLATFORM,
                      "requested config is unsupported on GBM");
         return NULL;
@@ -43,19 +46,23 @@ wgbm_config_choose(struct wcore_platform *wc_plat,
 }
 
 uint32_t
-wgbm_config_get_gbm_format(const struct wcore_config_attrs *attrs)
+wgbm_config_get_gbm_format(struct wcore_platform *wc_plat,
+                           struct wcore_display *wc_display,
+                           struct wcore_config *wc_config)
 {
-    if (attrs->red_size > 8 || attrs->blue_size > 8 ||
-        attrs->green_size > 8 || attrs->alpha_size > 8) {
+    EGLint gbm_format;
+    struct wgbm_display *dpy = wgbm_display(wc_display);
+    struct wegl_platform *plat = wegl_platform(wc_plat);
+    struct wegl_config *egl_config = wegl_config(wc_config);
+    bool ok = plat->eglGetConfigAttrib(dpy->wegl.egl,
+                                       egl_config->egl,
+                                       EGL_NATIVE_VISUAL_ID,
+                                       &gbm_format);
+
+    if (!ok) {
         return 0;
     }
-
-    if (attrs->alpha_size <= 0)
-        return GBM_FORMAT_XRGB8888;
-    else if (attrs->alpha_size <= 8)
-        return GBM_FORMAT_ABGR8888;
-
-    return 0;
+    return gbm_format;
 }
 
 union waffle_native_config*

src/waffle/gbm/wgbm_config.h
@@ -40,7 +40,9 @@ wgbm_config_choose(struct wcore_platform *wc_plat,
                    const struct wcore_config_attrs *attrs);
 
 uint32_t
-wgbm_config_get_gbm_format(const struct wcore_config_attrs *attrs);
+wgbm_config_get_gbm_format(struct wcore_platform *wc_plat,
+                           struct wcore_display *wc_dpy,
+                           struct wcore_config *wc_config);
 
 union waffle_native_config*
 wgbm_config_get_native(struct wcore_config *wc_config);

src/waffle/gbm/wgbm_window.c
@@ -72,7 +72,8 @@ wgbm_window_create(struct wcore_platform *wc_plat,
     if (self == NULL)
         return NULL;
 
-    gbm_format = wgbm_config_get_gbm_format(&wc_config->attrs);
+    gbm_format = wgbm_config_get_gbm_format(wc_plat, wc_config->display,
+                                            wc_config);
     assert(gbm_format != 0);
     self->gbm_surface = plat->gbm_surface_create(dpy->gbm_device,
                                                  width, height, gbm_format,

commit 0d71e456425ddf900073cd4408daa4c6751fbe51

gbm: Fix crash when eglChooseConfig fails
Crash introduced by 2bb939095c1d09c79dad5ab5da123ec77eb4ea61.

Also, stop calling eglChooseConfig twice.

Signed-off-by: Chad Versace <chad.versace@intel.com>

src/waffle/gbm/wgbm_config.c
@@ -35,14 +35,18 @@ wgbm_config_choose(struct wcore_platform *wc_plat,
                    struct wcore_display *wc_dpy,
                    const struct wcore_config_attrs *attrs)
 {
-    struct wcore_config *config = wegl_config_choose(wc_plat, wc_dpy, attrs);
-    if (wgbm_config_get_gbm_format(wc_plat, wc_dpy, config) == 0) {
+    struct wcore_config *wc_config = wegl_config_choose(wc_plat, wc_dpy, attrs);
+    if (!wc_config)
+        return NULL;
+
+    if (wgbm_config_get_gbm_format(wc_plat, wc_dpy, wc_config) == 0) {
         wcore_errorf(WAFFLE_ERROR_UNSUPPORTED_ON_PLATFORM,
                      "requested config is unsupported on GBM");
+        wegl_config_destroy(wc_config);
         return NULL;
     }
 
-    return wegl_config_choose(wc_plat, wc_dpy, attrs);
+    return wc_config;
 }
 
 uint32_t