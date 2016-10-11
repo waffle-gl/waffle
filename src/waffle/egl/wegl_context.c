waffle-gl/waffle

commit f8a485e7cc776b5f5ca25d02d6bd0a4704fa4c6a

egl: Improve support for robust GLES contexts on EGL 1.5
Allow the creation of robust GLES contexts when EGL 1.5 is available,
even when EGL_EXT_create_context_robustness is not.

Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
Signed-off-by: Chad Versace <chad.versace@intel.com>

src/waffle/egl/wegl_context.c
@@ -142,8 +142,14 @@ create_real_context(struct wegl_config *config,
             }
 
             if (attrs->context_robust) {
-                attrib_list[i++] = EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT;
-                attrib_list[i++] = EGL_TRUE;
+                // The EGL 1.5 token and the EXT token have different values.
+                if (dpy->major_version > 1 || dpy->minor_version >= 5) {
+                    attrib_list[i++] = EGL_CONTEXT_OPENGL_ROBUST_ACCESS;
+                    attrib_list[i++] = EGL_TRUE;
+                } else {
+                    attrib_list[i++] = EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT;
+                    attrib_list[i++] = EGL_TRUE;
+                }
             }
             break;
 

commit 64ca7f331038475f41917cba64682eb669493da5

egl: resolve build issue with pre EGL 1.5 headers
In some cases (like building on Android) the headers might not have the
1.5 definitions, leading to a build failures like the one in github
issue #41.

Since the ABI is baked in stone, add a local define to resolve that.

Fixes: f8a485e7cc7 ("egl: Improve support for robust GLES contexts on EGL 1.5")
Issue: https://github.com/waffle-gl/waffle/issues/41
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Chad Versace <chadversary@chromium.org>

src/waffle/egl/wegl_context.c
@@ -34,6 +34,11 @@
 #include "wegl_platform.h"
 #include "wegl_util.h"
 
+// Pre EGL 1.5 headers lack the definition.
+#ifndef EGL_CONTEXT_OPENGL_ROBUST_ACCESS
+#define EGL_CONTEXT_OPENGL_ROBUST_ACCESS  0x31B2
+#endif
+
 static bool
 bind_api(struct wegl_platform *plat, int32_t waffle_context_api)
 {