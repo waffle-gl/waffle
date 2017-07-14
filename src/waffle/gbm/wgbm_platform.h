1ded029d3ff72289b57c8463575c9e0696174e93
Jason Ekstrand <jason.ekstrand@intel.com>
2017-07-14 14:54:10 -0700

    gbm: Use modifiers to create the surface when available
    Reviewed-by: Chad Versace <chad@kiwitree.net>
    Closes: #45

--- src/waffle/gbm/wgbm_platform.h
+++ src/waffle/gbm/wgbm_platform.h
@@ -41,7 +41,8 @@
     f(struct gbm_surface *, gbm_surface_create               ,  true, (struct gbm_device *gbm, uint32_t width, uint32_t height, uint32_t format, uint32_t flags)) \
     f(void                , gbm_surface_destroy              ,  true, (struct gbm_surface *surface)) \
     f(struct gbm_bo *     , gbm_surface_lock_front_buffer    ,  true, (struct gbm_surface *surface)) \
-    f(void                , gbm_surface_release_buffer       ,  true, (struct gbm_surface *surface, struct gbm_bo *bo))
+    f(void                , gbm_surface_release_buffer       ,  true, (struct gbm_surface *surface, struct gbm_bo *bo)) \
+    f(struct gbm_surface *, gbm_surface_create_with_modifiers, false, (struct gbm_device *gbm, uint32_t width, uint32_t height, uint32_t format, const uint64_t *modifiers, const unsigned int count))
 
 struct linux_platform;
 
