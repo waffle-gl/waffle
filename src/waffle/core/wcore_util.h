e4c04db9e46928395973dbf3ecfe1990f0daf708
Emil Velikov <emil.l.velikov@gmail.com>
2021-07-13 00:19:04 +0100

    core: use Wpointer-arith safe container_of
    Copy Wpointer-arith safe implementation from mesa and toggle the
    warning in the meson build.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- src/waffle/core/wcore_util.h
+++ src/waffle/core/wcore_util.h
@@ -33,10 +33,8 @@
 extern "C" {
 #endif
 
-#define container_of(ptr, type, member) ({                              \
-        const __typeof__(((type *)0)->member ) *__mptr = (ptr);         \
-        (type*)((void*)__mptr - offsetof(type, member));                \
-     })
+#define container_of(ptr, type, member)                                 \
+    (type*)((uint8_t *)ptr - offsetof(type, member))
 
 /// @brief Safe downcast using container_of().
 ///
